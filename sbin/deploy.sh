fce8d033bd8d5d8f55c23dce23faae3da081d89a
Christian Hollinger <christian@chollinger.com>
2024-08-25 14:27:43 -0400

    fix: deploy.sh

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if [[ ! $UID -eq 0 ]]; then
 	echo "Script must be called as root"
 	exit 1
 fi
 
+WWW_ROOT=/var/www/
+BACKUP_DIR=$HOME/backups/blog/"$(date --iso)"
+
+mkdir -p "$BACKUP_DIR"
+
 cd "${DIR}/.."
 git submodule update --init --recursive && git submodule update --remote
 hugo --minify -d blog
-mv blog /var/www/
-chown -R www-data:www-data /var/www/blog
+mv "$WWW_ROOT/blog" "$BACKUP_DIR"
+rm -rf "$WWW_ROOT/blog"
+mv blog "$WWW_ROOT"
+chown -R www-data:www-data "$WWW_ROOT/blog"
